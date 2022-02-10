e6f2ca917175830bcbfd97818d8894327b3bd627
KieranHannagan <kieranhan1999@gmail.com>
2022-02-09 21:48:42 -0600

    deploying

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
+// mongoose.connect(
+//     process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false,
+//     },
+//   );
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
